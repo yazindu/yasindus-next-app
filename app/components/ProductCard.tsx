2dc47a6ba3ac59915818355c0a205e4054f74189
yasindu <yazindu@gmail.com>
2024-04-24 15:24:57 +0530

    Initial commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddToCart from "@/app/components/AddToCart";
+
+const ProductCard = () => {
+    return (
+        <div>
+            <AddToCart/>
+        </div>
+    );
+};
+
+export default ProductCard;
---
+++ app/users/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewUserPage = async () => {
+
+    return (
+        <div>
+            NewUserPage
+        </div>
+    );
+};
+
+export default NewUserPage;
