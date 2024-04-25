22697f10c82fea28fb04b6735d540591c3b01bf3
yasindu <yazindu@gmail.com>
2024-04-25 11:54:45 +0530

    learn: dynamic rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -8,12 +8,15 @@ interface User {
 const UsersPage = async () => {
     const res = await fetch(
         'https://jsonplaceholder.typicode.com/users',
-        // {cache: "no-cache"});
-        {next: {revalidate: 10}});
+    // );
+        {cache: "no-cache"});
+        //{next: {revalidate: 5}});
     const users: User[] = await res.json();
     return (
         <>
             <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}
+            </p>
             <ul>
                 {users.map(user => <li key={user.id}>{user.name}</li>)}
             </ul>
